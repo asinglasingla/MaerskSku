edf6ff665dbcaafcc761a65be0fff8d0e6b4bd3d
Ashish Singla <asingla.singla@gmail.com>
2021-01-18 23:30:49 -0800

    Added PromotionHandler which will: - apply the promotions on the cart - added test for this as well - added some code to program cs file to do inital testing on the solution

---
+++ MaerskSku/PromotionHandler.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+
+namespace MaerskSku
+{
+    public class PromotionHandler
+    {
+        private Promotion _promotion = null;
+        public decimal TotalValueAfterPromotion { get; private set; } = 0;
+        public void RegisterPromotion(Promotion promotion)
+        {
+            if (promotion != null)
+            {
+                _promotion = promotion;
+            }
+        }
+
+        public void Apply(Cart cart)
+        {
+            if (_promotion != null)
+            {
+                Promotion currentPromotion = _promotion;
+                while (currentPromotion != null)
+                {
+                    TotalValueAfterPromotion += currentPromotion.Calculate(cart);
+                    currentPromotion = currentPromotion.Successor;
+                }
+                foreach (var item in cart.CartItems.Where(x => x.Quantity > 0))
+                {
+                    TotalValueAfterPromotion += item.Quantity * item.UnitPrice;
+                }
+            }
+        }
+    }
+}
---
+++ MaerskSkuTest/PromotionHandlerTest.cs
@@ -0,0 +1,125 @@
+﻿using NUnit.Framework;
+using MaerskSku;
+using System.Collections.Generic;
+
+namespace MaerskSkuTest
+{
+    [TestFixture]
+    class PromotionHandlerTest
+    {
+        [Test]
+        public void PromotionHandler_Apply_ShouldApplyCorrectPromotions_OnCartItems_ScenarioA()
+        {
+            //Arrange
+            //Cart items
+            List<Sku> cartItems = new List<Sku> {
+            new Sku{SkuId = 'A', UnitPrice = 50, Quantity = 1},
+            new Sku{SkuId = 'B', UnitPrice = 30, Quantity = 1},
+            new Sku{SkuId = 'C', UnitPrice = 20, Quantity = 1}
+            };
+
+            Cart cart = new Cart();
+            cart.AddToCart(cartItems);
+
+            //Promotion to apply
+            Promotion promotion = new Promotion(130)
+            {
+                SkuInvolved = new List<Sku> { new Sku { SkuId = 'A', Quantity = 3 } },
+                Successor = new Promotion(45)
+                {
+                    SkuInvolved = new List<Sku> { new Sku { SkuId = 'B', Quantity = 2 } },
+                    Successor = new Promotion(30)
+                    {
+                        SkuInvolved = new List<Sku> {
+                            new Sku { SkuId = 'C', Quantity = 1 } ,
+                            new Sku { SkuId = 'D', Quantity = 1 }
+                        }
+                    }
+                }
+            };
+
+            //Act
+            PromotionHandler handler = new PromotionHandler();
+            handler.RegisterPromotion(promotion);
+            handler.Apply(cart);
+            Assert.True(handler.TotalValueAfterPromotion == 100);
+        }
+
+        [Test]
+        public void PromotionHandler_Apply_ShouldApplyCorrectPromotions_OnCartItems_ScenarioB()
+        {
+            //Arrange
+            //Cart items
+            List<Sku> cartItems = new List<Sku> {
+            new Sku{SkuId = 'A', UnitPrice = 50, Quantity = 5},
+            new Sku{SkuId = 'B', UnitPrice = 30, Quantity = 5},
+            new Sku{SkuId = 'C', UnitPrice = 20, Quantity = 1}
+            };
+
+            Cart cart = new Cart();
+            cart.AddToCart(cartItems);
+
+            //Promotion to apply
+            Promotion promotion = new Promotion(130)
+            {
+                SkuInvolved = new List<Sku> { new Sku { SkuId = 'A', Quantity = 3 } },
+                Successor = new Promotion(45)
+                {
+                    SkuInvolved = new List<Sku> { new Sku { SkuId = 'B', Quantity = 2 } },
+                    Successor = new Promotion(30)
+                    {
+                        SkuInvolved = new List<Sku> {
+                            new Sku { SkuId = 'C', Quantity = 1 } ,
+                            new Sku { SkuId = 'D', Quantity = 1 }
+                        }
+                    }
+                }
+            };
+
+            //Act
+            PromotionHandler handler = new PromotionHandler();
+            handler.RegisterPromotion(promotion);
+            handler.Apply(cart);
+            Assert.True(handler.TotalValueAfterPromotion == 370);
+        }
+
+        [Test]
+        public void PromotionHandler_Apply_ShouldApplyCorrectPromotions_OnCartItems_ScenarioC()
+        {
+            //Arrange
+            //Cart items
+            List<Sku> cartItems = new List<Sku> {
+            new Sku{SkuId = 'A', UnitPrice = 50, Quantity = 3},
+            new Sku{SkuId = 'B', UnitPrice = 30, Quantity = 5},
+            new Sku{SkuId = 'C', UnitPrice = 20, Quantity = 1},
+            new Sku{SkuId = 'D', UnitPrice = 15, Quantity = 1}
+            };
+
+            Cart cart = new Cart();
+            cart.AddToCart(cartItems);
+
+            //Promotion to apply
+            Promotion promotion = new Promotion(130)
+            {
+                SkuInvolved = new List<Sku> { new Sku { SkuId = 'A', Quantity = 3 } },
+                Successor = new Promotion(45)
+                {
+                    SkuInvolved = new List<Sku> { new Sku { SkuId = 'B', Quantity = 2 } },
+                    Successor = new Promotion(30)
+                    {
+                        SkuInvolved = new List<Sku> {
+                            new Sku { SkuId = 'C', Quantity = 1 } ,
+                            new Sku { SkuId = 'D', Quantity = 1 }
+                        }
+                    }
+                }
+            };
+
+            //Act
+            PromotionHandler handler = new PromotionHandler();
+            handler.RegisterPromotion(promotion);
+            handler.Apply(cart);
+            Assert.True(handler.TotalValueAfterPromotion == 280);
+        }
+    }
+}
