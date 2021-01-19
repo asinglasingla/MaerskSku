f00f1369103e4804eab1094cb46b9d7df2228f83
Ashish Singla <asingla.singla@gmail.com>
2021-01-19 00:17:10 -0800

    added comments for better understanding

--- MaerskSku/Promotion.cs
+++ MaerskSku/Promotion.cs
@@ -21,15 +21,19 @@ namespace MaerskSku
                 return totalValue;
             }
 
+            //filter out items from the cart and Sku's involved in the promotion
             var itemsEligibleForPromotion = cart.CartItems?.Join(SkuInvolved, cartItem => cartItem.SkuId, prom => prom.SkuId, (outer, inner) => new { cart = outer, prom = inner }).
                 Where(x => x.cart.Quantity >= x.prom.Quantity);
 
             if (itemsEligibleForPromotion != null && itemsEligibleForPromotion.Count() == SkuInvolved.Count())
             {
+                //if out of 10 items in the cart 2 are eligible then this will run 2 times.
                 foreach (var item in itemsEligibleForPromotion)
                 {
+                    //will execute till there is not chance of applying promotion left, which mean items in the cart are now not eligible for this promotion
                     while (item.cart.Quantity >= item.prom.Quantity)
                     {
+                        //reduce the quantity of Sku in the cart which all are involved in the promotion with its corresponding promotion quantity
                         foreach (var itemForPromo in itemsEligibleForPromotion)
                         {
                             itemForPromo.cart.Quantity -= itemForPromo.prom.Quantity;
