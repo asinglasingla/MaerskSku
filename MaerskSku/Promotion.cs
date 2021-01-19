2b20b51296c1e203019167b6d5737052830fc333
Ashish Singla <asingla.singla@gmail.com>
2021-01-18 23:23:50 -0800

    added Promotion  class which will: - have the value to apply for that promotion - maintain all the Sku involved in the promotion - also maintain a chain of other promotions, which can be applied on the cart one by one - will calculate the value after applying promotion on the cart items if applicable

---
+++ MaerskSku/Promotion.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace MaerskSku
+{
+    public class Promotion
+    {
+        public Promotion(decimal valueAfterPromotion)
+        {
+            ValueAfterPromotion = valueAfterPromotion;
+        }
+        public decimal ValueAfterPromotion { get; }
+        public IEnumerable<Sku> SkuInvolved { get; set; }
+        public Promotion Successor { get; set; }
+
+        public decimal Calculate(Cart cart)
+        {
+            decimal totalValue = 0;
+            if (cart == null || SkuInvolved == null)
+            {
+                return totalValue;
+            }
+
+            var itemsEligibleForPromotion = cart.CartItems?.Join(SkuInvolved, cartItem => cartItem.SkuId, prom => prom.SkuId, (outer, inner) => new { cart = outer, prom = inner }).
+                Where(x => x.cart.Quantity >= x.prom.Quantity);
+
+            if (itemsEligibleForPromotion != null && itemsEligibleForPromotion.Count() == SkuInvolved.Count())
+            {
+                foreach (var item in itemsEligibleForPromotion)
+                {
+                    while (item.cart.Quantity >= item.prom.Quantity)
+                    {
+                        foreach (var itemForPromo in itemsEligibleForPromotion)
+                        {
+                            itemForPromo.cart.Quantity -= itemForPromo.prom.Quantity;
+                        }
+                        totalValue += ValueAfterPromotion;
+                    }
+                }
+            }
+            return totalValue;
+        }
+    }
+}
---
+++ MaerskSkuTest/PromotionTest.cs
@@ -0,0 +1,91 @@
+﻿using NUnit.Framework;
+using MaerskSku;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace MaerskSkuTest
+{
+    [TestFixture]
+    class PromotionTest
+    {
+        private Promotion _promotion = null;
+
+        [SetUp]
+        public void Setup()
+        {
+
+            _promotion = new Promotion(130)
+            {
+                SkuInvolved = new List<Sku> { new Sku { SkuId = 'A', Quantity = 3 } },
+                Successor = new Promotion(45)
+                {
+                    SkuInvolved = new List<Sku> { new Sku { SkuId = 'B', Quantity = 2 } },
+                    Successor = new Promotion(30)
+                    {
+                        SkuInvolved = new List<Sku> {
+                            new Sku { SkuId = 'C', Quantity = 2 } ,
+                            new Sku { SkuId = 'D', Quantity = 1 }
+                        }
+                    }
+                }
+            };
+        }
+
+        [Test]
+        public void Promotion_Calculate_ShouldReturnZeroIfPromotionIsNotApplicable()
+        {
+            //Arrange
+            //Sku present in the cart
+            List<Sku> cartItems = new List<Sku> {
+            new Sku{SkuId = 'A', UnitPrice = 50, Quantity = 1},
+            new Sku{SkuId = 'B', UnitPrice = 30, Quantity = 1},
+            new Sku{SkuId = 'C', UnitPrice = 20, Quantity = 1}
+            };
+
+            //Confifured cart based on scenarios
+            Cart cart = new Cart();
+            cart.AddToCart(cartItems);
+
+            //Act
+            decimal valueAfterApplyingPromotion = _promotion.Calculate(cart);
+            Assert.True(valueAfterApplyingPromotion == 0);
+        }
+
+        //[TestCase('A', 50, 5, ExpectedResult = 130)]
+        //[TestCase('B', 30, 5, ExpectedResult = 90)]
+        [TestCase('C', 20, 1, ExpectedResult = 0)]
+        public decimal Promotion_Calculate_Should_ApplyPromotionFor_(char sku, decimal unitPrice, int quantity)
+        {
+            //Confifured cart based on scenarios
+            Cart cart = new Cart();
+            cart.AddToCart(new Sku { SkuId = sku, UnitPrice = unitPrice, Quantity = quantity });
+
+            //Act
+            //It will calculate only for the first cart item, because Promotion handler class is iterating over all cart items one by one
+
+            Promotion specificPromotion = FindPromotion(sku, _promotion);
+
+            //Also for only C or D it will not apply the promotion, as per promotion for C + D at least one of both should be there,
+            //else it will just return 0. And Sku specific costs is handled in PromotionHandler.
+            var result = specificPromotion.Calculate(cart);
+            return result;
+        }
+
+        private Promotion FindPromotion(char sku, Promotion promotion)
+        {
+            if (promotion == null)
+            {
+                return new Promotion(0) { SkuInvolved = null, Successor = null };
+            }
+
+            if (promotion.SkuInvolved.Any(x => x.SkuId == sku))
+            {
+                return promotion;
+            }
+            else
+            {
+                return FindPromotion(sku, promotion.Successor);
+            }
+        }
+    }
+}
